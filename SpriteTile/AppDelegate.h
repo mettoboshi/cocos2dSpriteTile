04497f1c23dac8e7023902fdbc89715318085199
mettoboshi <mettoboshi.1108@gmail.com>
2013-02-11 20:11:47 +0900

    Initial Commit

---
+++ SpriteTile/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  SpriteTile
+//
+//  Created by mettoboshi on 13/02/11.
+//  Copyright mettobo 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
